2763d6c9c41fc6477ed8ee763f26297676f3b692
alexxijielu <alexlu@cs.toronto.edu>
2018-05-28 11:03:53 -0400

    Fixed bugs around ellfit.mcv file

--- batch_segmentation.py
+++ batch_segmentation.py
@@ -29,57 +29,65 @@ if __name__ == '__main__':
 
     images = os.listdir(inputdir)
     for image in images:
-        ID = image.split(".")[0]
+        if image.endswith(".tif"):
+            ID = image.split(".")[0]
 
-        # Parse tiff files for red channels
-        command = PMbin + "PMTiffManip -f 2,4,6,8 " + inputdir + image + " " + inputdir + ID + "_red.tif"
-        subprocess.call(shlex.split(command))
+            # Parse tiff files for red channels
+            command = PMbin + "PMTiffManip -f 2,4,6,8 " + inputdir + image + " " + inputdir + ID + "_red.tiff"
+            subprocess.call(shlex.split(command))
 
-        # Identify cell centers
-        command = PMbin + "PMSegmentation -B 1.0f -b " + inputdir + ID + ".scp -d " + inputdir + ID + "_dist.tif " + \
-                  inputdir + ID + "_red.tif " + inputdir + ID + "_seg.tif"
-        subprocess.call(shlex.split(command))
+            # Seperate Background and Foreground in Images
+            command = PMbin + "PMSegmentation -B 1.0f -b " + inputdir + ID + ".scp -d " + inputdir + ID + "_dist.tiff " + \
+                      inputdir + ID + "_red.tiff " + inputdir + ID + "_seg.tiff"
+            subprocess.call(shlex.split(command))
 
-        # Find cell sub-segments
-        command = PMbin + "PMWaterShed -M -b 1.0 " + inputdir + ID + "_red.tif " + inputdir + ID + "_watershed.tif"
-        subprocess.call(shlex.split(command))
+            # Identify cell centers
+            command = PMbin + "PMFindMultiCover -o " + inputdir + ID + "_ellfit.mcv -R2 1.0 6.0 " + \
+                      inputdir + ID + "_red.tiff " + inputdir + ID + "_seg.tiff " + inputdir + ID + "_dist.tiff "
+            subprocess.call(shlex.split(command))
 
-        # Cell identification from circle coordinated directed sub-segment agglomeration
-        command = PMbin + "PMHiddenMapDirect -M -G " + inputdir + ID + "_watershed.tif " + "-C " +\
-                  inputdir + ID + "_cellseg.tif " + inputdir + ID + "_seg.tif " + inputdir + ID + \
-                  "_dist.tif " + inputdir + ID + "_ellfit.tif "
-        subprocess.call(shlex.split(command))
+            # Find cell sub-segments
+            command = PMbin + "PMWaterShed -M -b 1.0 " + inputdir + ID + "_red.tiff " + inputdir + ID + "_watershed.tiff"
+            subprocess.call(shlex.split(command))
 
-        # Parse tiff files for "green" channel
-        command = PMbin + "PMTiffManip -f 1,3,5,7 " + inputdir + image + " " + inputdir + ID + "_gre.tif"
-        subprocess.call(shlex.split(command))
+            # Cell identification from circle coordinated directed sub-segment agglomeration
+            command = PMbin + "PMHiddenMapDirect -M -G " + inputdir + ID + "_watershed.tiff " + "-C " +\
+                      inputdir + ID + "_cellseg.tiff " + inputdir + ID + "_seg.tiff " + inputdir + ID + \
+                      "_dist.tiff " + inputdir + ID + "_ellfit.mcv "
+            subprocess.call(shlex.split(command))
 
-        # Copy temporary version of confidence measure
-        command = "cp ./Conf_Matrices.scp " + inputdir + ID + "_conf_matrices.scp"
-        subprocess.call(shlex.split(command))
+            # Parse tiff files for "green" channel
+            command = PMbin + "PMTiffManip -f 1,3,5,7 " + inputdir + image + " " + inputdir + ID + "_gre.tiff"
+            subprocess.call(shlex.split(command))
 
-        # Measure GFP Intensity and spatial spread
-        command = PMbin + "PMExtractFeatures -c " + inputdir + ID + "_conf_matrices.scp -m " + \
-                  inputdir + ID + "_ellfit.tif -a 0.1 10 0.9 0.1 50 -t " + inputdir + ID + ".txt -S " + \
-                  inputdir + ID + "_seg.tif " + inputdir + ID + "_red.tif " +\
-                  inputdir + ID + "_gre.tif " + inputdir + ID + "_cellseg.tif"
-        subprocess.call(shlex.split(command))
+            # Copy temporary version of confidence measure
+            command = "cp ./Conf_Matrices.scp " + inputdir + ID + "_conf_matrices.scp"
+            subprocess.call(shlex.split(command))
 
-        # Produce a Displayable RGB Tiff file showing the cell boundary and bud necks:
-        command = PMbin + "PMMakeDisplay " + inputdir + ID + ".txt " + inputdir + ID + "_cellseg.tif " \
-                  + inputdir + ID + "_red.tif " + inputdir + ID + "_gre.tif " + inputdir + ID + "_preview.tif "
-        subprocess.call(shlex.split(command))
+            # Measure GFP Intensity and spatial spread
+            command = PMbin + "PMExtractFeatures -c " + inputdir + ID + "_conf_matrices.scp -m " + \
+                      inputdir + ID + "_ellfit.mcv -a 0.1 10 0.9 0.1 50 -t " + inputdir + ID + ".txt -S " + \
+                      inputdir + ID + "_seg.tiff " + inputdir + ID + "_red.tiff " +\
+                      inputdir + ID + "_gre.tiff " + inputdir + ID + "_cellseg.tiff"
+            subprocess.call(shlex.split(command))
 
-        # Clean up intermediate files
-        command = "rm " + inputdir + ID + "_red.tif"
-        subprocess.call(shlex.split(command))
-        command = "rm " + inputdir + ID + "_gre.tif"
-        subprocess.call(shlex.split(command))
-        command = "rm " + inputdir + ID + "_dist.tif"
-        subprocess.call(shlex.split(command))
-        command = "rm " + inputdir + ID + ".scp"
-        subprocess.call(shlex.split(command))
-        command = "rm " + inputdir + ID + "_watershed.tif"
-        subprocess.call(shlex.split(command))
-        command = "rm " + inputdir + ID + "_conf_matrices.scp"
-        subprocess.call(shlex.split(command))
+            # Produce a Displayable RGB Tiff file showing the cell boundary and bud necks:
+            command = PMbin + "PMMakeDisplay " + inputdir + ID + ".txt " + inputdir + ID + "_cellseg.tiff " \
+                      + inputdir + ID + "_red.tiff " + inputdir + ID + "_gre.tiff " + inputdir + ID + "_preview.tiff "
+            subprocess.call(shlex.split(command))
+
+            # Clean up intermediate files
+            command = "rm " + inputdir + ID + "_red.tiff"
+            subprocess.call(shlex.split(command))
+            command = "rm " + inputdir + ID + "_gre.tiff"
+            subprocess.call(shlex.split(command))
+            command = "rm " + inputdir + ID + "_dist.tiff"
+            subprocess.call(shlex.split(command))
+            command = "rm " + inputdir + ID + ".scp"
+            subprocess.call(shlex.split(command))
+            command = "rm " + inputdir + ID + "_watershed.tiff"
+            subprocess.call(shlex.split(command))
+            command = "rm " + inputdir + ID + "_conf_matrices.scp"
+            subprocess.call(shlex.split(command))
+            command = "rm " + inputdir + ID + "_ellfit.mcv"
+            subprocess.call(shlex.split(command))
